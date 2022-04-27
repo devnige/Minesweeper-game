7d44ea074cbfc9ae97d73d67089761fcc87f48d2
devnige <nigel.quinlan@myob.com>
2022-04-27 22:33:04 +1000

    rebuilt tests project

---
+++ MineSweeperTests/FakeConsoleOut.cs
@@ -0,0 +1,36 @@
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using MinesweeperGame.Output;
+
+namespace MineSweeperTests
+{
+    public class FakeConsoleOut : TextWriter, ITypeWriter
+    {
+        public readonly Dictionary<string, int> _writtenStrings = new ();
+        
+        public override Encoding Encoding { get; }
+        
+        public override void Write(string str)
+        {
+            AddStringToWrittenString(str);
+        }
+
+        private void AddStringToWrittenString(string str)
+        {
+            if (_writtenStrings.ContainsKey(str))
+            {
+                _writtenStrings[str]++;
+            }
+            else
+            {
+                _writtenStrings.Add(str, 1);
+            }
+        }
+
+        public void Typewrite(string message)
+        {
+            AddStringToWrittenString(message);
+        }
+    }
+}
