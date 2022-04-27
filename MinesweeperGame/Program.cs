345633c809842ac7bca0a8ad58d9b5358876fb85
devnige <nigel.quinlan@myob.com>
2022-04-27 16:15:06 +1000

    add output dependency to TypeWriter

--- MinesweeperGame/Program.cs
+++ MinesweeperGame/Program.cs
@@ -1,17 +1,10 @@
 ﻿using System;
-using System.CodeDom.Compiler;
-using System.IO;
-using System.Text;
-using System.Threading;
-using System.Threading.Channels;
-using System.Xml;
-using MinesweeperGame.Output;
 
 namespace MinesweeperGame
 {
-    static class Program
+    internal static class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             var game = new Game(Console.In, Console.Out, new Random(), new TypeWriter(Console.Out));
             game.Run();
